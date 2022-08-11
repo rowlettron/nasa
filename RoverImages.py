d4ff52addd15d8757a437a5706611d7196a5fe7c
rowlettron <rowlettron@yahoo.com>
2022-08-10 20:27:26 -0500

    Create image json file and updates to RoverImages.py

--- RoverImages.py
+++ RoverImages.py
@@ -31,25 +31,62 @@ def main():
 
     currentDirectory = os.getcwd()
 
+    api_key = 'CAejpD9hOfWhaGxZyv2p6bS9A7HmBP1Sf53Vp8yi'
+
     if platform == "darwin":
         os_platform = "Mac"
         excelFile = currentDirectory + "/" + rover + ".xlsx"
         baseImageFolder = "/Users/ron/OneDrive/Photos/NASA/"
+        roverFolder = baseImageFolder + rover + "/"
+        roverDateFolder = roverFolder + earth_date + "/"
+        roverCameraFolder = roverDateFolder + camera + "/"
+
+        if not os.path.exists(roverDateFolder):
+            os.makedirs(roverDateFolder)
+
+        if not os.path.exists(roverCameraFolder):
+            os.makedirs(roverCameraFolder)
+
         clearConsole()
     else:
         os_platform = "Windows"
         excelFile = currentDirectory + "\\" + rover + ".xlsx"
         baseImageFolder = "C:\\Users\\rowle\\OneDrive\\Photos\\NASA\\"
+        roverFolder = baseImageFolder + rover + "\\"
+        roverDateFolder = roverFolder + earth_date + "\\"
+        roverCameraFolder = roverDateFolder + camera + "\\"
+
+        if not os.path.exists(roverDateFolder):
+            os.makedirs(roverDateFolder)
+
+        if not os.path.exists(roverCameraFolder):
+            os.makedirs(roverCameraFolder)
+
         clearConsole()
 
+    print('Rover: {0}', rover)
+    print('Camera: {0}', camera)
+    print('EarthDate: {0}', earth_date)
 
-def buildFolderStructure():
-    print('')
+    data = getAPIData(rover, earth_date, camera, api_key)
+
+    print(data)
+
+def getAPIData(rover, earth_date, camera, api_key):
+    params = {'earth_date': earth_date, 'camera': camera, 'api_key': api_key}   
+
+    url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/' + rover + '/photos?'
+
+    print(url)
+
+    data = requests.get(url, params=params)
+
+    data = json.loads(data.text)
+
+    return data 
 
 
 ################# Main Processing Section ##############################
 
 if __name__ == '__main__':
-    start_date = '2022-07-10'
-    end_date = '2022-07-20'
     main()
